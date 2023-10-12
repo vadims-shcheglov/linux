60b204c56c1c7a8d3274d85702ad7d45d36c39c7
Vadim Shcheglov <147752227+vadims-shcheglov@users.noreply.github.com>
2023-10-12 21:35:30 +0300

    Create init.el
    The basics of configuration emacs

---
+++ init.el
@@ -0,0 +1,9 @@
+;; Don't show the splash screen
+(setq inhibit-startup-message t)
+(setq inhibit-startup-message t)
+;; Don't show the splash screen
+
+;; Turn off some unneeded UI elements
+(menu-bar-mode nil)
+(tool-bar-mode nil)
+(scroll-bar-mode nil)
