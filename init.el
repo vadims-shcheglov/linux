1857e9ebb1041406d4d91ee23145c0addde24b95
Vadim Shcheglov <147752227+vadims-shcheglov@users.noreply.github.com>
2023-10-13 12:12:32 +0300

    Update init.el

--- init.el
+++ init.el
@@ -1,9 +1,14 @@
 ;; Don't show the splash screen
 (setq inhibit-startup-message t)
-(setq inhibit-startup-message t)
-;; Don't show the splash screen
+;; Don't want any backup files
+(setq make-backup-files nil)
+;; Don't want any .saves files
+(setq auto-save-list-file-name nil)
+;; Don't want any auto saving
+(setq auto-save-default nil)
 
-;; Turn off some unneeded UI elements
-(menu-bar-mode nil)
-(tool-bar-mode nil)
-(scroll-bar-mode nil)
+;; Display line numbers in every buffer
+(global-display-line-numbers-mode t)
+
+;; Global keyboard shortcuts
+(global-set-key (kbd "C-c a") (lambda() (interactive)(find-file "~/.emacs")))
