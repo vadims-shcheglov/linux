c4ae65fe5ca57ef9f9f72aa92f2450ad369b45a5
Vadim Shcheglov <147752227+vadims-shcheglov@users.noreply.github.com>
2023-10-14 14:05:59 +0300

    Update main.c

--- main.c
+++ main.c
@@ -1,12 +1,30 @@
-#include <ncurses.h>
+/* main.c
+ *
+ * Copyright 2023 Vadim Shcheglov
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
 
-int main()
+#include <stdio.h>
+#include <stdlib.h>
+
+int
+main (int argc,
+      char *argv[])
 {
-    initscr();                 /* Start curses mode     */
-    printw("Hello World !!!"); /* Print Hello World    */
-    refresh();                 /* Print it on to the real screen */
-    getch();                   /* Wait for user input */
-    endwin();                  /* End curses mode    */
-
-    return 0;
+  printf ("Hello, World!\n");
+  return EXIT_SUCCESS;
 }
