f1a86cee51f4bd4476d2fc49dc299bd7b1839f4f
Vadim Shcheglov <147752227+vadims-shcheglov@users.noreply.github.com>
2023-10-14 14:02:39 +0300

    Update init.el

--- init.el
+++ init.el
@@ -1,17 +1,15 @@
-;; Don't show the splash screen
-(setq inhibit-startup-message t)
-;; Don't want any backup files
-(setq make-backup-files nil)
-;; Don't want any .saves files
-(setq auto-save-list-file-name nil)
-;; Don't want any auto saving
-(setq auto-save-default nil)
+(setq inhibit-startup-message    t) ; Don'n show the splash screen
+(setq make-backup-files        nil) ; Don't want any backup files
+(setq auto-save-list-file-name nil) ; Don't want any .saves files
+(setq auto-save-default        nil) ; Don't want any auto saving
 
 ;; Display line numbers in every buffer
 (global-display-line-numbers-mode t)
 
 ;; Global keyboard shortcuts
-(global-set-key (kbd "C-c a") (lambda() (interactive)(find-file "~/.emacs")))
-(global-set-key (kbd "C-c q") (lambda() (interactive)(find-file "~/emacs/init.el")))
-(global-set-key (kbd "C-c w") (lambda() (interactive)(find-file "~/source/Makefile")))
-(global-set-key (kbd "C-c e") (lambda() (interactive)(find-file "~/source/main.c")))
+(global-set-key (kbd "C-c a") 'compile)
+(global-set-key (kbd "C-c q") (lambda() (interactive)(find-file "~/source/main.c")))
+(global-set-key (kbd "C-c w") (lambda() (interactive)(find-file "~/.emacs")))
+(global-set-key (kbd "C-z") (lambda() (interactive)(find-file "~/emacs/init.el")))
+
+(provide 'init)
